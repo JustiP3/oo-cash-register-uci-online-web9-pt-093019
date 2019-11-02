99db789d31f601a95c6b5b5c3a9b743f6c72bc98
Justin Phillips <justinphil02@yahoo.com>
2019-11-02 01:24:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+class CashRegister
 
+attr_accessor :total, :discount
+
+
+def initialize(emp_disc=0)
+  @total = 0.0
+  @discount = emp_disc
+end
+
+def add_item(title, price, qty=1) #wtf are we doing with title ??
+  @total += price*qty
+
+end
+
+def apply_discount
+  if @discount != 0
+new_total = (@total * (100-@discount) / 100)
+#discount is an integer representing a percentage
+@total = new_total
+"After the discount, the total comes to $#{@total.to_i}."
+else
+  "There is no discount to apply."
+end #end of if block
+end # end of method apply_discount
+
+
+
+
+end # end of class cash register
